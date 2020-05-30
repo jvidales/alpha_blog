4f1adf93d9ce7c2a6d9e3c114c3f34fa98f84c53
Jesus Vidales <jvidales@me.com>
2020-05-30 17:18:53 -0500

    Finished Code

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'services', to: 'pages#services'
-  resources :articles, only: [:show]
+  resources :articles
 end
