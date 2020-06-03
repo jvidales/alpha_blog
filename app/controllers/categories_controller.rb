a25e513263a962cc1e57df1a2c5ae39939a75e9a
Jesus Vidales <jvidales@me.com>
2020-06-03 01:37:09 -0500

    Added categories and views

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,40 @@
+class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
+
+    def show
+        @category = Category.find(params[:id])
+        @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index
+        @categories = Category.paginate(page: params[:page], per_page: 5)
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category successfully created"
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert] = "Not enought permissions"
+            redirect_to categories_path
+        end
+    end
+
+end
---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ApplicationRecord
+    has_many :article_categories
+    has_many :articles, through: :article_categories
+    validates :name, presence: true, length: {in: 3..25}
+    validates_uniqueness_of :name
+end
